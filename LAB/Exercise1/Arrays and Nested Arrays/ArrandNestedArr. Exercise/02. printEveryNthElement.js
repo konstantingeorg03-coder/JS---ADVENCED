00f56d387ab9d361f52f6652e52d126ac4cc6663
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-09-15 19:44:00 +0300

    add the exercise printEveryNthElement, from JS ADVANCED 1, ArrandNestedArr. Exercise 1

---
+++ LAB/Exercise1/Arrays and Nested Arrays/ArrandNestedArr. Exercise/02. printEveryNthElement.js
@@ -0,0 +1,15 @@
+function printEveryNthElement(arr, num){
+    let result = [];
+    for(let i = 0; i < arr.length; i += num){
+        result.push(arr[i]);
+    }
+
+    return result;
+}
+
+console.log(printEveryNthElement(['5', 
+'20', 
+'31', 
+'4', 
+'20'], 
+2));
