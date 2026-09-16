6a51abd714cbde8d6f16fc04cff8a81ee72271ee
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-09-16 20:49:27 +0300

    add the exercise cityRecord from JS ADVANCED 1, Objects - Exercise

---
+++ LAB/Exercise1/Arrays and Nested Arrays/ArrandNestedArr. Exercise/Objects - Lab/01. cityRecord.js
@@ -0,0 +1,13 @@
+ function cityRecord(name, population, treasury){
+    let output = {
+        name: name,
+        population: population,
+        treasury: treasury
+    }
+
+    return output;
+}
+
+console.log(cityRecord('Tortuga',
+7000,
+15000));
