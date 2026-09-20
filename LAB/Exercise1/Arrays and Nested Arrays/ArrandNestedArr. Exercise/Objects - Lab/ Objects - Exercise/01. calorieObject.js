3ab5f05f6563c82cba03325a785d193559e86c0b
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-09-20 16:15:49 +0300

    add the exercise caloerieObject from Objects - Exercise

---
+++ LAB/Exercise1/Arrays and Nested Arrays/ArrandNestedArr. Exercise/Objects - Lab/ Objects - Exercise/01. calorieObject.js
@@ -0,0 +1,16 @@
+function calorieObject(arr){
+    let result = {};
+
+    for(let i = 0; i < arr.length; i += 2){
+        let food = arr[i];
+
+        let calories = Number(arr[i + 1]);
+
+        result[food] = calories;
+    }
+
+    console.log(result);
+}
+
+calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
+
