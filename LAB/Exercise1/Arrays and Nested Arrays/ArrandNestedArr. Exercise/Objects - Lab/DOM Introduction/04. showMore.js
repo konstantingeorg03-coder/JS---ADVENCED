ba01cba7799c732da6b95e65384adf23f3dbbf26
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-09-21 23:00:17 +0300

    add thhe exercise showMore from DOM Introduction

---
+++ LAB/Exercise1/Arrays and Nested Arrays/ArrandNestedArr. Exercise/Objects - Lab/DOM Introduction/04. showMore.js
@@ -0,0 +1,9 @@
+function showText(){
+    let hiddenText = document.getElementById('text');
+
+    let showMore = document.getElementById('more');
+
+    hiddenText.style.display = 'inline';
+
+    showMore.style.display = 'none';
+}
