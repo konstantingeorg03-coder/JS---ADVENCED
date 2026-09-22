e54209b0673f4155e14d871dceb911e6d4bfb654
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-09-22 15:27:58 +0300

    add the exercise pascalOrCamelCase from DOM Introduction - Exercise

--- LAB/Exercise1/Arrays and Nested Arrays/ArrandNestedArr. Exercise/Objects - Lab/DOM Introduction/DOM Inreoduction - Exercise/02. pascalOrcamelCase.js
+++ LAB/Exercise1/Arrays and Nested Arrays/ArrandNestedArr. Exercise/Objects - Lab/DOM Introduction/DOM Inreoduction - Exercise/02. pascalOrcamelCase.js
@@ -1,3 +1,35 @@
 function solve(){
-    
+    let firstInput = document.getElementById('text');
+
+    let secondInput = document.getElementById('naming-convention');
+
+    let resultElement = document.getElementById('result');
+
+    let firstText = firstInput.value;
+
+    let secondText = secondInput.value;
+
+    firstText = firstText.toLowerCase();
+
+    firstText = firstText.split(' ');
+
+    if(secondText === 'Camel Case'){
+        for(let i = 1; i < firstText.length; i++){
+            firstText[i] = firstText[i][0].toUpperCase() + firstText[i].slice(1);
+        }
+
+        firstText = firstText.join('');
+
+    }else if(secondText === 'Pascal Case'){
+        for(let i = 0; i < firstText.length; i++){
+            firstText[i] = firstText[i][0].toUpperCase() + firstText[i].slice(1);
+        }
+
+        firstText = firstText.join('');
+
+    }else{
+        firstText = 'Error!';
+    }
+
+    resultElement.textContent = firstText;
 }
