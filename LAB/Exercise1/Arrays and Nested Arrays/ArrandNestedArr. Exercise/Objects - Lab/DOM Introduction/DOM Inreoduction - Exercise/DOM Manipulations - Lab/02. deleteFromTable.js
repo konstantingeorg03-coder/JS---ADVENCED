edd2e9071cf91dc9ef4d379354059d3ea1a04e00
Konstantin Georgiev <konstantingeorg03@gmail.com>
2026-09-25 00:34:55 +0300

    add the exercise deleteFromtable solution 2 from DOM Manipulstions

--- LAB/Exercise1/Arrays and Nested Arrays/ArrandNestedArr. Exercise/Objects - Lab/DOM Introduction/DOM Inreoduction - Exercise/DOM Manipulations - Lab/02. deleteFromTable.js
+++ LAB/Exercise1/Arrays and Nested Arrays/ArrandNestedArr. Exercise/Objects - Lab/DOM Introduction/DOM Inreoduction - Exercise/DOM Manipulations - Lab/02. deleteFromTable.js
@@ -1,27 +1,27 @@
-function deleteByEmail(){
-    let input = document.querySelector('[name = "email"]');
-
-    let inputValue = input.value;
+function deleteByEmail() {
+    let input = document.querySelector('[name="email"]');
+    let usersInput = input.value;
 
     let result = document.getElementById('result');
 
-    let arr = Array.from(document.querySelectorAll('tbody tr'));
+    let rows = document.querySelectorAll('tbody tr');
+    let arr = Array.from(rows);
 
     let isFound = false;
 
-    for(let row of arr){
-        if(row.querySelectorAll('td')[1].textContent === inputValue){
+    for (let row of arr) {
+        let cells = row.querySelectorAll('td');
+        let email = cells[1].textContent;
+
+        if (email === usersInput) {
             row.remove();
-
             result.textContent = 'Deleted.';
-
             isFound = true;
-
             break;
         }
     }
 
-    if(isFound === false){
+    if (isFound === false) {
         result.textContent = 'Not found.';
     }
 }
